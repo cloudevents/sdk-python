efed0f78cc5cca25649371c13156fbd62db225dc
Curtis Mason <31265687+cumason123@users.noreply.github.com>
2020-06-26 16:52:37 -0400

    Http structured cloudevents (#47)
    * Moved fields out of base & structured support
    
    base._ce_required_fields and base._ce_optional_fields were moved into
    event classes v03 and v1.
    
    http_events.CloudEvent class now looks for fieldnames in either headers
    or data, and can automatically determine whether this is a binary or
    structured event.
    
    Signed-off-by: Curtis Mason <cumason@google.com>
    
    * testing structured
    
    Signed-off-by: Curtis Mason <cumason@google.com>
    
    * added tests for structured events
    
    Signed-off-by: Curtis Mason <cumason@google.com>
    
    * Added test valid structured cloudevents
    
    Signed-off-by: Curtis Mason <cumason@google.com>
    
    * Created default headers arg in CloudEvent
    
    Signed-off-by: Curtis Mason <cumason@google.com>
    
    * Added http_events.py sample code
    
    Signed-off-by: Curtis Mason <cumason@google.com>
    
    * removed ../python-event-requests
    
    Signed-off-by: Curtis Mason <cumason@google.com>
    
    * README.md nit
    
    Signed-off-by: Curtis Mason <cumason@google.com>
    
    * client.py nit
    
    Signed-off-by: Curtis Mason <cumason@google.com>
    
    * comment nits
    
    Signed-off-by: Curtis Mason <cumason@google.com>
    
    * created __getitem__ in CloudEvent
    
    Signed-off-by: Curtis Mason <cumason@google.com>
    
    * sample nits
    
    Signed-off-by: Curtis Mason <cumason@google.com>
    
    * fixed structured empty data issue
    
    Signed-off-by: Curtis Mason <cumason@google.com>
    
    * Added CloudEvent to README
    
    Signed-off-by: Curtis Mason <cumason@google.com>
    
    * added http_msg to CloudEvent
    
    Signed-off-by: Curtis Mason <cumason@google.com>
    
    * implemented ToRequest in CloudEvent
    
    Signed-off-by: Curtis Mason <cumason@google.com>
    
    * testing more specversions
    
    Signed-off-by: Curtis Mason <cumason@google.com>
    
    * Added sample code to README.md
    
    Signed-off-by: Curtis Mason <cumason@google.com>
    
    * modified sample code
    
    Signed-off-by: Curtis Mason <cumason@google.com>
    
    * added datavalidation to changelog
    
    Signed-off-by: Curtis Mason <cumason@google.com>
    
    * updated README
    
    Signed-off-by: Curtis Mason <cumason@google.com>
    
    * README adjustment
    
    Signed-off-by: Curtis Mason <cumason@google.com>
    
    * ruler 80 adjustment on http_events
    
    Signed-off-by: Curtis Mason <cumason@google.com>
    
    * style and renamed ToRequest to to_request
    
    Signed-off-by: Curtis Mason <cumason@google.com>
    
    * lint fix
    
    Signed-off-by: Curtis Mason <cumason@google.com>
    
    * fixed self.binary typo
    
    Signed-off-by: Curtis Mason <cumason@google.com>
    
    * CHANGELOG adjustment
    
    Signed-off-by: Curtis Mason <cumason@google.com>
    
    * rollback CHANGELOG
    
    Signed-off-by: Curtis Mason <cumason@google.com>
    
    * Added documentation to to_request
    
    Signed-off-by: Curtis Mason <cumason@google.com>
    
    * README.md adjustment
    
    Signed-off-by: Curtis Mason <cumason@google.com>
    
    * renamed event_handler to event_version
    
    Signed-off-by: Curtis Mason <cumason@google.com>
    
    * inlined field_name_modifier
    
    Signed-off-by: Curtis Mason <cumason@google.com>
    
    * renamed test body data
    
    Signed-off-by: Curtis Mason <cumason@google.com>
    
    * removed unnecessary headers from test
    
    Signed-off-by: Curtis Mason <cumason@google.com>
    
    * removed field_name_modifier and fixed e.g. in client.py
    
    Signed-off-by: Curtis Mason <cumason@google.com>
    
    * pylint fix
    
    Signed-off-by: Curtis Mason <cumason@google.com>

---
+++ samples/http-cloudevents/client.py
@@ -0,0 +1,80 @@
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+import sys
+import io
+from cloudevents.sdk.http_events import CloudEvent
+
+import requests
+
+
+def send_binary_cloud_event(url):
+    # define cloudevents data
+    headers = {
+        "ce-id": "binary-event-id",
+        "ce-source": "localhost",
+        "ce-type": "template.http.binary",
+        "ce-specversion": "1.0",
+        "Content-Type": "application/json",
+        "ce-time": "2018-10-23T12:28:23.3464579Z"
+    }
+    data = {"payload-content": "Hello World!"}
+
+    # create a CloudEvent 
+    event = CloudEvent(data, headers=headers)
+    headers, body = event.to_request()
+
+    # send and print event
+    requests.post(url, headers=headers, json=body)
+    print(
+        f"Sent {event['ce-id']} from {event['ce-source']} with "
+        f"{event['data']}"
+    )
+
+
+def send_structured_cloud_event(url):
+    # define cloudevents data
+    data = {
+        "id": "structured-event-id",
+        "source": "localhost",
+        "type": "template.http.structured",
+        "specversion": "1.0",
+        "Content-Type": "application/json",
+        "time": "2018-10-23T12:28:23.3464579Z",
+        "data": {
+            "payload-content": "Hello World!"
+        }
+    }
+
+    # create a CloudEvent 
+    event = CloudEvent(data)
+    headers, body = event.to_request()
+
+    # send and print event
+    requests.post(url, headers=headers, json=body)
+    print(
+        f"Sent {event['ce-id']} from {event['ce-source']} with "
+        f"{event['data']}"
+    )
+
+
+if __name__ == "__main__":
+    # expects a url from command line.
+    # e.g. python3 client.py http://localhost:3000/
+    if len(sys.argv) < 2:
+        sys.exit("Usage: python with_requests.py "
+                 "<CloudEvents controller URL>")
+
+    url = sys.argv[1]
+    send_binary_cloud_event(url)
+    send_structured_cloud_event(url)
