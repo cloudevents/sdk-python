f17fe55cd26e566b405ec17c283562533364972b
Alexander Tkachev <sasha64sasha@gmail.com>
2022-07-09 23:22:44 +0300

    docs: fix bad grammar
    Signed-off-by: Alexander Tkachev <sasha64sasha@gmail.com>

--- cloudevents/http/event.py
+++ cloudevents/http/event.py
@@ -81,11 +81,14 @@ class CloudEvent:
         self, key: str, default: typing.Optional[typing.Any] = None
     ) -> typing.Optional[typing.Any]:
         """
-        Get a an attribute value, return default if does not exist.
-        MUST NOT throw exception when the key does not exist
-        :param key: attribute name
-        :param default: value which will be returned if the attribute does not exist
-        :returns: attribute value if exists, default otherwise
+        Retrieves an event attribute value for the given key.
+        Returns the default value if not attribute for the given key exists.
+
+        MUST NOT throw an exception when the key does not exist.
+
+        :param key: The event attribute name.
+        :param default: The default value to be returned when no attribute with the given key exists.
+        :returns: The event attribute value if exists, default value otherwise.
         """
         return self._attributes.get(key, default)
 
