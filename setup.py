dc37abfeda575f9e63493374bc9923a091f9cfd4
Yurii Serhiichuk <savik.ne@gmail.com>
2022-07-10 13:33:12 +0300

    chore: unify copyright with other SDKs and update/add it where needed.
    Signed-off-by: Yurii Serhiichuk <savik.ne@gmail.com>

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-# All Rights Reserved.
+#  Copyright 2018-Present The CloudEvents Authors
 #
 #    Licensed under the Apache License, Version 2.0 (the "License"); you may
 #    not use this file except in compliance with the License. You may obtain
@@ -11,6 +11,7 @@
 #    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 #    License for the specific language governing permissions and limitations
 #    under the License.
+
 import codecs
 import os
 import pathlib
