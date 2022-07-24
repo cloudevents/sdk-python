a8872b9808ce5fa6fad09369820fe078d6c3c8b9
Alexander Tkachev <sasha64sasha@gmail.com>
2022-07-25 01:06:06 +0300

    test: remove broken tests
    Signed-off-by: Alexander Tkachev <sasha64sasha@gmail.com>

--- cloudevents/tests/test_abstract_cloudevent.py
+++ cloudevents/tests/test_abstract_cloudevent.py
@@ -3,22 +3,6 @@ import pytest
 from cloudevents.abstract import CloudEvent
 
 
-def test_del_is_abstract():
-    """
-    exists mainly for coverage reasons
-    """
-    with pytest.raises(NotImplementedError):
-        del CloudEvent()["a"]
-
-
-def test_set_is_abstract():
-    """
-    exists mainly for coverage reasons
-    """
-    with pytest.raises(NotImplementedError):
-        CloudEvent()["a"] = 2
-
-
 def test_create_is_abstract():
     """
     exists mainly for coverage reasons
