617dda67cca8ae61ef3615d9a0da9eaa079e41d1
Yurii Serhiichuk <savik.ne@gmail.com>
2022-08-14 18:02:19 +0300

    chore: sort imports.
    Signed-off-by: Yurii Serhiichuk <savik.ne@gmail.com>

--- cloudevents/http/http_methods.py
+++ cloudevents/http/http_methods.py
@@ -23,15 +23,27 @@
 #    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 #    License for the specific language governing permissions and limitations
 #    under the License.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
 
 import typing
 
 from deprecation import deprecated
 
 from cloudevents.abstract import AnyCloudEvent
-from cloudevents.http.conversion import from_http as _moved_from_http
 from cloudevents.conversion import to_binary as _moved_to_binary
 from cloudevents.conversion import to_structured as _moved_to_structured
+from cloudevents.http.conversion import from_http as _moved_from_http
 from cloudevents.http.event import CloudEvent
 from cloudevents.sdk import types
 
