657c66c0db4c2e8f9a72f1a883b2a72ad4839c12
Yurii Serhiichuk <savik.ne@gmail.com>
2022-08-14 18:19:15 +0300

    chore: Define `__all__`
    Signed-off-by: Yurii Serhiichuk <savik.ne@gmail.com>

--- cloudevents/http/__init__.py
+++ cloudevents/http/__init__.py
@@ -35,6 +35,18 @@
 #    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 #    License for the specific language governing permissions and limitations
 #    under the License.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
 
 
 from cloudevents.conversion import to_binary, to_dict, to_json, to_structured
@@ -44,3 +56,7 @@ from cloudevents.http.http_methods import to_binary_http  # deprecated
 from cloudevents.http.http_methods import to_structured_http  # deprecated
 from cloudevents.sdk.converters.binary import is_binary
 from cloudevents.sdk.converters.structured import is_structured
+
+__all__ = [to_json, to_binary, to_dict, to_structured, from_json, from_dict, CloudEvent,
+           is_binary, is_structured,
+           to_binary_http, to_structured_http]
