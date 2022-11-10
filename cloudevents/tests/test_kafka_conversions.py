cb6c0a21e928ddf4f4de62d25528635b2de5261e
davidwmartines <d5172@yahoo.com>
2022-11-10 16:53:57 -0600

    Add tests for to/from structured.
    Signed-off-by: davidwmartines <d5172@yahoo.com>

--- cloudevents/tests/test_kafka_conversions.py
+++ cloudevents/tests/test_kafka_conversions.py
@@ -18,6 +18,7 @@ import json
 
 import pytest
 
+from cloudevents import exceptions as cloud_exceptions
 from cloudevents.http import CloudEvent
 from cloudevents.kafka.conversion import (
     ProtocolMessage,
@@ -26,11 +27,22 @@ from cloudevents.kafka.conversion import (
     to_binary,
     to_structured,
 )
-from cloudevents.sdk.event.attribute import SpecVersion
+from cloudevents.sdk import types
+
+expected_data = {"name": "test", "amount": 1}
+
+
+# simple serdes for testing marshallers
+def _serialize(data: dict) -> bytes:
+    return bytes(json.dumps(data).encode("utf-8"))
+
+
+def _deserialize(data: bytes) -> dict:
+    return json.loads(data.decode())
 
 
 @pytest.fixture
-def source_event():
+def source_event() -> CloudEvent:
     return CloudEvent.create(
         attributes={
             "specversion": "1.0",
@@ -41,12 +53,12 @@ def source_event():
             "content-type": "foo",
             "key": "test_key_123",
         },
-        data={"name": "test", "amount": 1},
+        data=expected_data,
     )
 
 
 @pytest.fixture
-def source_binary_json_message():
+def source_binary_json_message() -> ProtocolMessage:
     return ProtocolMessage(
         headers={
             "ce_specversion": "1.0".encode("utf-8"),
@@ -58,13 +70,13 @@ def source_binary_json_message():
             .encode("utf-8"),
             "content-type": "foo".encode("utf-8"),
         },
-        value=json.dumps({"name": "test", "amount": 1}).encode("utf-8"),
+        value=json.dumps(expected_data).encode("utf-8"),
         key="test_key_123",
     )
 
 
 @pytest.fixture
-def source_binary_bytes_message():
+def source_binary_bytes_message() -> ProtocolMessage:
     return ProtocolMessage(
         headers={
             "ce_specversion": "1.0".encode("utf-8"),
@@ -76,20 +88,116 @@ def source_binary_bytes_message():
             .encode("utf-8"),
             "content-type": "foo".encode("utf-8"),
         },
-        value=bytes("hello".encode("utf-8")),
+        value=_serialize(expected_data),
         key="test_key_123",
     )
 
 
-def test_to_binary_set_value_default_marshaller(source_event):
+@pytest.fixture
+def source_structured_json_message() -> ProtocolMessage:
+    return ProtocolMessage(
+        headers={
+            "content-type": "foo".encode("utf-8"),
+        },
+        value=json.dumps(
+            {
+                "specversion": "1.0",
+                "id": "1234-1234-1234",
+                "source": "pytest",
+                "type": "com.pytest.test",
+                "time": datetime.datetime(2000, 1, 1, 6, 42, 33).isoformat(),
+                "data": expected_data,
+            }
+        ).encode("utf-8"),
+        key="test_key_123",
+    )
+
+
+@pytest.fixture
+def source_structured_json_bytes_message() -> ProtocolMessage:
+    return ProtocolMessage(
+        headers={
+            "content-type": "foo".encode("utf-8"),
+        },
+        value=json.dumps(
+            {
+                "specversion": "1.0",
+                "id": "1234-1234-1234",
+                "source": "pytest",
+                "type": "com.pytest.test",
+                "time": datetime.datetime(2000, 1, 1, 6, 42, 33).isoformat(),
+                "data_base64": base64.b64encode(_serialize(expected_data)).decode(
+                    "ascii"
+                ),
+            }
+        ).encode("utf-8"),
+        key="test_key_123",
+    )
+
+
+@pytest.fixture
+def source_structured_bytes_bytes_message() -> ProtocolMessage:
+    return ProtocolMessage(
+        headers={
+            "content-type": "foo".encode("utf-8"),
+        },
+        value=_serialize(
+            {
+                "specversion": "1.0",
+                "id": "1234-1234-1234",
+                "source": "pytest",
+                "type": "com.pytest.test",
+                "time": datetime.datetime(2000, 1, 1, 6, 42, 33).isoformat(),
+                "data_base64": base64.b64encode(_serialize(expected_data)).decode(
+                    "ascii"
+                ),
+            }
+        ),
+        key="test_key_123",
+    )
+
+
+@pytest.fixture
+def custom_marshaller() -> types.MarshallerType:
+    return _serialize
+
+
+@pytest.fixture
+def custom_unmarshaller() -> types.MarshallerType:
+    return _deserialize
+
+
+def _throw():
+    raise Exception("fail")
+
+
+@pytest.fixture
+def bad_marshaller() -> types.MarshallerType:
+    return _throw
+
+
+# sanity check for tests that use custom marshalling
+def test_custom_marshaller_can_talk_to_itself(custom_marshaller, custom_unmarshaller):
+    data = expected_data
+    marshalled = custom_marshaller(data)
+    unmarshalled = custom_unmarshaller(marshalled)
+    for k, v in data.items():
+        assert unmarshalled[k] == v
+
+
+def test_throw_raises():
+    with pytest.raises(Exception):
+        _throw()
+
+
+def test_to_binary_sets_value_default_marshaller(source_event):
     result = to_binary(source_event)
     assert result.value == json.dumps(source_event.data).encode("utf-8")
 
 
-def test_to_binary_set_value_custom_marshaller(source_event):
-    marshaller = lambda data: bytes(str(data).encode("utf-8"))
-    result = to_binary(source_event, data_marshaller=marshaller)
-    assert result.value == bytes(str(source_event.data).encode("utf-8"))
+def test_to_binary_sets_value_custom_marshaller(source_event, custom_marshaller):
+    result = to_binary(source_event, data_marshaller=custom_marshaller)
+    assert result.value == custom_marshaller(source_event.data)
 
 
 def test_to_binary_sets_key(source_event):
@@ -100,13 +208,13 @@ def test_to_binary_sets_key(source_event):
 def test_to_binary_none_key(source_event):
     source_event["key"] = None
     result = to_binary(source_event)
-    assert result.key == None
+    assert result.key is None
 
 
 def test_to_binary_no_key(source_event):
     del source_event["key"]
     result = to_binary(source_event)
-    assert result.key == None
+    assert result.key is None
 
 
 def test_to_binary_sets_headers(source_event):
@@ -125,17 +233,23 @@ def test_to_binary_sets_headers(source_event):
     assert "key" not in result.headers
 
 
+def test_to_binary_raise_marshaller_exception(source_event, bad_marshaller):
+    with pytest.raises(cloud_exceptions.DataMarshallerError):
+        to_binary(source_event, data_marshaller=bad_marshaller)
+
+
 def test_from_binary_default_marshaller(source_binary_json_message):
     result = from_binary(source_binary_json_message)
     assert result.data == json.loads(source_binary_json_message.value.decode())
 
 
-def test_from_binary_custom_marshaller(source_binary_bytes_message):
-    unmarshaller = lambda data: data.decode()
+def test_from_binary_custom_marshaller(
+    source_binary_bytes_message, custom_unmarshaller
+):
     result = from_binary(
-        source_binary_bytes_message, CloudEvent, data_unmarshaller=unmarshaller
+        source_binary_bytes_message, data_unmarshaller=custom_unmarshaller
     )
-    assert result.data == source_binary_bytes_message.value.decode()
+    assert result.data == custom_unmarshaller(source_binary_bytes_message.value)
 
 
 def test_from_binary_sets_key(source_binary_json_message):
@@ -167,3 +281,237 @@ def test_from_binary_sets_attrs_from_headers(source_binary_json_message):
         result["content-type"]
         == source_binary_json_message.headers["content-type"].decode()
     )
+
+
+def test_from_binary_unmarshaller_exception(source_binary_json_message, bad_marshaller):
+    with pytest.raises(cloud_exceptions.DataUnmarshallerError):
+        from_binary(source_binary_json_message, data_unmarshaller=bad_marshaller)
+
+
+def test_binary_can_talk_to_itself(source_event):
+    message = to_binary(source_event)
+    event = from_binary(message)
+    for key, val in source_event.get_attributes().items():
+        assert event[key] == val
+    for key, val in source_event.data.items():
+        assert event.data[key] == val
+
+
+def test_binary_can_talk_to_itself_custom_marshaller(
+    source_event, custom_marshaller, custom_unmarshaller
+):
+    message = to_binary(source_event, data_marshaller=custom_marshaller)
+    event = from_binary(message, data_unmarshaller=custom_unmarshaller)
+    for key, val in source_event.get_attributes().items():
+        assert event[key] == val
+    for key, val in source_event.data.items():
+        assert event.data[key] == val
+
+
+def test_to_structured_sets_value_default_marshallers(source_event):
+    result = to_structured(source_event)
+    assert result.value == json.dumps(
+        {
+            "specversion": source_event["specversion"],
+            "id": source_event["id"],
+            "source": source_event["source"],
+            "type": source_event["type"],
+            "time": source_event["time"],
+            "data": expected_data,
+        }
+    ).encode("utf-8")
+
+
+def test_to_structured_sets_value_custom_data_marshaller_default_envelope(
+    source_event, custom_marshaller
+):
+    result = to_structured(source_event, data_marshaller=custom_marshaller)
+    assert result.value == json.dumps(
+        {
+            "specversion": source_event["specversion"],
+            "id": source_event["id"],
+            "source": source_event["source"],
+            "type": source_event["type"],
+            "time": source_event["time"],
+            "data_base64": base64.b64encode(custom_marshaller(expected_data)).decode(
+                "ascii"
+            ),
+        }
+    ).encode("utf-8")
+
+
+def test_to_structured_sets_value_custom_envelope_marshaller(
+    source_event, custom_marshaller
+):
+    result = to_structured(source_event, envelope_marshaller=custom_marshaller)
+    assert result.value == custom_marshaller(
+        {
+            "specversion": source_event["specversion"],
+            "id": source_event["id"],
+            "source": source_event["source"],
+            "type": source_event["type"],
+            "time": source_event["time"],
+            "data": expected_data,
+        }
+    )
+
+
+def test_to_structured_sets_value_custom_marshallers(source_event, custom_marshaller):
+    result = to_structured(
+        source_event,
+        data_marshaller=custom_marshaller,
+        envelope_marshaller=custom_marshaller,
+    )
+    assert result.value == custom_marshaller(
+        {
+            "specversion": source_event["specversion"],
+            "id": source_event["id"],
+            "source": source_event["source"],
+            "type": source_event["type"],
+            "time": source_event["time"],
+            "data_base64": base64.b64encode(custom_marshaller(expected_data)).decode(
+                "ascii"
+            ),
+        }
+    )
+
+
+def test_to_structured_sets_key(source_event):
+    result = to_structured(source_event)
+    assert result.key == source_event["key"]
+
+
+def test_to_structured_none_key(source_event):
+    source_event["key"] = None
+    result = to_structured(source_event)
+    assert result.key is None
+
+
+def test_to_structured_no_key(source_event):
+    del source_event["key"]
+    result = to_structured(source_event)
+    assert result.key is None
+
+
+def test_to_structured_sets_headers(source_event):
+    result = to_structured(source_event)
+    assert len(result.headers) == 1
+    assert result.headers["content-type"] == source_event["content-type"].encode(
+        "utf-8"
+    )
+
+
+def test_to_structured_datamarshaller_exception(source_event, bad_marshaller):
+    with pytest.raises(cloud_exceptions.DataMarshallerError):
+        to_structured(source_event, data_marshaller=bad_marshaller)
+
+
+def test_to_structured_envelope_datamarshaller_exception(source_event, bad_marshaller):
+    with pytest.raises(cloud_exceptions.DataMarshallerError):
+        to_structured(source_event, envelope_marshaller=bad_marshaller)
+
+
+def test_structured_can_talk_to_itself(source_event):
+    message = to_structured(source_event)
+    event = from_structured(message)
+    for key, val in source_event.get_attributes().items():
+        assert event[key] == val
+    for key, val in source_event.data.items():
+        assert event.data[key] == val
+
+
+def test_from_structured_sets_data_default_data_unmarshaller(
+    source_structured_json_message,
+):
+    result = from_structured(source_structured_json_message)
+    assert result.data == expected_data
+
+
+def test_from_structured_sets_data_custom_data_unmarshaller(
+    source_structured_json_bytes_message, custom_unmarshaller
+):
+    result = from_structured(
+        source_structured_json_bytes_message, data_unmarshaller=custom_unmarshaller
+    )
+    assert result.data == expected_data
+
+
+def test_from_structured_sets_data_custom_unmarshallers(
+    source_structured_bytes_bytes_message, custom_unmarshaller
+):
+    result = from_structured(
+        source_structured_bytes_bytes_message,
+        data_unmarshaller=custom_unmarshaller,
+        envelope_unmarshaller=custom_unmarshaller,
+    )
+    assert result.data == expected_data
+
+
+def test_from_structured_sets_attrs_default_enveloper_unmarshaller(
+    source_structured_json_message,
+):
+    result = from_structured(source_structured_json_message)
+    for key, value in json.loads(source_structured_json_message.value.decode()).items():
+        if key != "data":
+            assert result[key] == value
+
+
+def test_from_structured_sets_attrs_custom_enveloper_unmarshaller(
+    source_structured_bytes_bytes_message,
+    custom_unmarshaller,
+):
+    result = from_structured(
+        source_structured_bytes_bytes_message,
+        data_unmarshaller=custom_unmarshaller,
+        envelope_unmarshaller=custom_unmarshaller,
+    )
+    for key, value in custom_unmarshaller(
+        source_structured_bytes_bytes_message.value
+    ).items():
+        if key not in ["data_base64"]:
+            assert result[key] == value
+
+
+def test_from_structured_sets_content_type_default_envelope_unmarshaller(
+    source_structured_json_message,
+):
+    result = from_structured(source_structured_json_message)
+    assert (
+        result["content-type"]
+        == source_structured_json_message.headers["content-type"].decode()
+    )
+
+
+def test_from_structured_sets_content_type_custom_envelope_unmarshaller(
+    source_structured_bytes_bytes_message, custom_unmarshaller
+):
+    result = from_structured(
+        source_structured_bytes_bytes_message,
+        data_unmarshaller=custom_unmarshaller,
+        envelope_unmarshaller=custom_unmarshaller,
+    )
+    assert (
+        result["content-type"]
+        == source_structured_bytes_bytes_message.headers["content-type"].decode()
+    )
+
+
+def test_from_structured_data_unmarshaller_exception(
+    source_structured_bytes_bytes_message, custom_unmarshaller, bad_marshaller
+):
+    with pytest.raises(cloud_exceptions.DataUnmarshallerError):
+        from_structured(
+            source_structured_bytes_bytes_message,
+            data_unmarshaller=bad_marshaller,
+            envelope_unmarshaller=custom_unmarshaller,
+        )
+
+
+def test_from_structured_envelope_unmarshaller_exception(
+    source_structured_bytes_bytes_message, bad_marshaller
+):
+    with pytest.raises(cloud_exceptions.DataUnmarshallerError):
+        from_structured(
+            source_structured_bytes_bytes_message,
+            envelope_unmarshaller=bad_marshaller,
+        )
