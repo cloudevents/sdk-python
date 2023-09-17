b2e84cdaf87957e16f047b878b59e7575180d3bc
Federico Busetti <729029+febus982@users.noreply.github.com>
2023-09-17 17:48:07 +0100

    Use SDK json deserialization logic
    Signed-off-by: Federico Busetti <729029+febus982@users.noreply.github.com>

--- cloudevents/pydantic/v2/event.py
+++ cloudevents/pydantic/v2/event.py
@@ -12,16 +12,18 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
-import base64
 import datetime
 import json
 import typing
+from typing import Any
+
+from pydantic.deprecated import parse as _deprecated_parse
 
 from cloudevents.exceptions import PydanticFeatureNotInstalled
 from cloudevents.pydantic.fields_docs import FIELD_DESCRIPTIONS
 
 try:
-    from pydantic import BaseModel, ConfigDict, Field, model_serializer, model_validator
+    from pydantic import BaseModel, ConfigDict, Field, model_serializer
 except ImportError:  # pragma: no cover # hard to test
     raise PydanticFeatureNotInstalled(
         "CloudEvents pydantic feature is not installed. "
@@ -152,20 +154,39 @@ class CloudEvent(abstract.CloudEvent, BaseModel):  # type: ignore
         },
     )
 
-    @model_validator(mode="before")
+    """
+    We should use a @model_validator decorator to handle JSON deserialisation,
+    however it's not possible to completely bypass the internal pydantic logic
+    and still use the CloudEvents shared conversion logic.
+
+    Same issue applies to the multiple from/to JSON conversion logic in the
+    @model_serializer implemented after
+
+    To remove the need for the multiple from/to JSON transformation we need
+    major refactor in the SDK conversion logic.
+    """
+
     @classmethod
-    def check_base64_data_input(cls, data: typing.Any) -> typing.Any:
-        """Populates the `data` property if the model gets created using `data_base64`.
+    def model_validate_json(
+        cls,
+        json_data: typing.Union[str, bytes, bytearray],
+        *,
+        strict: typing.Optional[bool] = None,
+        context: typing.Optional[typing.Dict[str, Any]] = None,
+    ) -> "CloudEvent":
+        return conversion.from_json(cls, json_data)
 
-        :param data: Input data.
-
-        :return: Event serialized as a standard CloudEvent dict with user specific
-        parameters.
-        """
-        if isinstance(data, dict) and data.get("data_base64") is not None:
-            data["data"] = base64.b64decode(data["data_base64"])
-            del data["data_base64"]
-        return data
+    @classmethod
+    def parse_raw(
+        cls,
+        b: typing.Union[str, bytes],
+        *,
+        content_type: typing.Optional[str] = None,
+        encoding: str = "utf8",
+        proto: typing.Optional[_deprecated_parse.Protocol] = None,
+        allow_pickle: bool = False,
+    ) -> "CloudEvent":
+        return conversion.from_json(cls, b)
 
     @model_serializer(when_used="json")
     def _ce_json_dumps(self) -> typing.Dict[str, typing.Any]:
@@ -180,10 +201,6 @@ class CloudEvent(abstract.CloudEvent, BaseModel):  # type: ignore
         :return: Event serialized as a standard CloudEvent dict with user specific
         parameters.
         """
-        # This is inefficient but we want to use the same serialization logic
-        # as the rest of the SDK. We need either for pydantic to allow bypassing
-        # the internal JSON serialization logic, or for the conversion module to
-        # separate the fields structured data conversion from the json serialization
         return json.loads(conversion.to_json(self))
 
     def _get_attributes(self) -> typing.Dict[str, typing.Any]:
